afe0ba636ea6b83f08da7ffb95c677a339ab0c6c
Yu-Sheng Chen <143129316+YSChen0609@users.noreply.github.com>
2026-05-15 12:03:35 +0800

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.py
@@ -0,0 +1,34 @@
+# backtracking
+# use the nums list itself
+# form the perm step by step until len(perm) == len(nums)
+# for each step, pick a num and append it, then pop it from the list
+
+# time: O(n!)
+# space: O(n) - extra space
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        l = len(nums)
+
+        def backtrack(perm, n_list):
+            if len(perm) == l:
+                res.append(perm.copy())
+                return
+
+            for i, n in enumerate(n_list):
+                perm.append(n)
+                n_list.pop(i)
+
+                backtrack(perm,n_list)
+                
+                n_list.insert(i, n)
+                perm.pop()
+
+        backtrack([], nums)
+        return res
+
+
+
+
+
