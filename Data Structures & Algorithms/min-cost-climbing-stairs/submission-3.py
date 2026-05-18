6fd1022b0ca18a9b4fa54e94d21ac00c5225af6e
Yu-Sheng Chen <143129316+YSChen0609@users.noreply.github.com>
2026-05-18 10:03:17 +0800

    Add: min-cost-climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-3.py
@@ -0,0 +1,20 @@
+# dp
+# go forward until (top_idx)
+# dp[i] = min(dp[i-1], dp[i-2]) + cost[i]
+# time: O(N)
+# space: O(N) - could be further reduced
+# reduce space complexity:
+# to O(1)
+# curr = min(prev_1, prev_2) + cost[i]
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        cost.append(0) # target step costs nothing
+        dp = [0] * (len(cost))
+        prev_2, prev_1 = cost[0], cost[1]
+
+        for i in range(2, len(dp)):
+            curr = min(prev_2, prev_1) + cost[i]
+            prev_2, prev_1 = prev_1, curr
+
+        return curr
